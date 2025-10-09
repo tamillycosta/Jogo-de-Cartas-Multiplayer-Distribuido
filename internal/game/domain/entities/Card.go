8e3dd2f8652b74cba42683a84cf786e78e910b86
Tamilly costa <Tamillycosta899@gmail.com>
2025-10-08 22:36:51 -0300

    feat: implementa entidades do jogo

--- internal/game/domain/entities/Card.go
+++ internal/game/domain/entities/Card.go
@@ -6,12 +6,15 @@ import (
 
 
 type Card struct {
-    ID       string `gorm:"type:char(36);primaryKey" json:"id"`
-    Name     string `gorm:"size:100;not null" json:"name"`
-    Type     string `gorm:"size:50" json:"type"`
-    Power    int    `gorm:"default:0" json:"power"`
-    Rarity   string `gorm:"size:20" json:"rarity"`
-    PlayerID *string `gorm:"type:char(36);index" json:"player_id,omitempty"`
+    ID        string  `gorm:"type:char(36);primaryKey" json:"id"`
+    Name      string  `gorm:"size:100;not null" json:"name"`
+    Type      string  `gorm:"size:50" json:"type"`
+    Power     int     `gorm:"default:0" json:"power"`
+    Rarity    string  `gorm:"size:20" json:"rarity"`
+
+    // Relações (somente uma pode ser != nil)
+    PlayerID  *string `gorm:"type:char(36);index" json:"player_id,omitempty"`
     PackageID *string `gorm:"type:char(36);index" json:"package_id,omitempty"`
+
     CreatedAt time.Time `json:"created_at"`
 }
--- internal/game/domain/entities/Package.go
+++ internal/game/domain/entities/Package.go
@@ -1,9 +1,16 @@
 package entities
 
+import(
+	"time"
+)
 
-type Package struct{
-	Id  string   `gorm:"type:char(36);primaryKey" json:"id"`
-	Cards       []*Card   `gorm:"foreignKey:PackgeID" json:"cards,omitempty"`
-	Status    string `gorm:"size:20"` // available, locked, opened
-	LockedBy  string `gorm:"size:50"` 
+
+
+type Package struct {
+    ID        string   `gorm:"type:char(36);primaryKey" json:"id"`
+    Cards     []*Card  `gorm:"foreignKey:PackageID" json:"cards,omitempty"`
+    Status    string   `gorm:"size:20"`  // "available", "locked", "opened"
+    LockedBy  *string  `gorm:"type:char(36);index" json:"locked_by,omitempty"` // PlayerID que bloqueou
+    CreatedAt time.Time
+    UpdatedAt time.Time
 }

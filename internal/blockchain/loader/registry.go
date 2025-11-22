a2ad9c1f2cec14a8a6a6762a286440caee822fb7
Tamilly costa <Tamillycosta899@gmail.com>
2025-11-22 20:24:49 -0300

    fix: bug no caminho do arquivo json

--- internal/blockchain/loader/registry.go
+++ internal/blockchain/loader/registry.go
@@ -4,22 +4,33 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"Jogo-de-Cartas-Multiplayer-Distribuido/internal/blockchain/contracts"
 	client "Jogo-de-Cartas-Multiplayer-Distribuido/internal/blockchain"
+	
 )
 // Os contratos gerados pelo abigen (codigo solidity -> byteCode -> golang)
 type Contracts struct {
 	Package *blockchain.PackageRegistry
+	Card *blockchain.Card
 	
 }
 
 func LoadAllContracts(cli *client.BlockchainClient, networkID string) (*Contracts, error) {
-	// carrega o endereço do contrato atravez do loader
-	packageAddrStr, _ := LoadContractAddress("build/contracts/PackageRegistry.json", networkID)
-	// card
-	// player 
-	pkg, _ := blockchain.NewPackageRegistry(common.HexToAddress(packageAddrStr), cli.Client)
+	// Adicionar o endereço real dos contratos criados que serão apresentados na interface do ganache
+	// ou acessar pelo truffle console (PackageRegistry.address)
+	packageAddr := common.HexToAddress("0x5b1869D9A4C187F2EAa108f3062412ecf0526b24") 
+	cardAddr := common.HexToAddress("0xe78A0F7E598Cc8b0Bb87894B0F60dD2a88d6a8Ab")   
+	
+	pkg, err := blockchain.NewPackageRegistry(packageAddr, cli.Client)
+	if err != nil {
+		return nil, err
+	}
+	
+	card, err := blockchain.NewCard(cardAddr, cli.Client)
+	if err != nil {
+		return nil, err
+	}
 	
 	return &Contracts{
 		Package: pkg,
-		
+		Card:    card,
 	}, nil
 }

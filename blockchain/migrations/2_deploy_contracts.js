d505665bdf47b74818c6f0f6707f146fe555b9cd
Tamilly costa <Tamillycosta899@gmail.com>
2025-11-22 20:11:26 -0300

    feat: adiciona contrato para as cartas no migrations

--- blockchain/migrations/2_deploy_contracts.js
+++ blockchain/migrations/2_deploy_contracts.js
@@ -1,7 +1,10 @@
-// faz o deploy do contrato (truffle)
-
+const Card = artifacts.require("Card");
 const PackageRegistry = artifacts.require("PackageRegistry");
 
-module.exports = function (deployer) {
-  deployer.deploy(PackageRegistry);
+module.exports = async function (deployer) {
+    await deployer.deploy(Card);
+    const card = await Card.deployed();
+
+    // Agora sim: passa o endereço do contrato Card
+    await deployer.deploy(PackageRegistry, card.address);
 };
